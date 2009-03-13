4a5c9e6702916b25e42e7967cb52a1f0bcab5a9a
s.astanin <s.astanin@b7d359d8-80ec-11dd-8d42-05603b9c63e9>
2009-03-13 11:19:46 +0000

    An API version 0.1 for the web version of the script.
    POST /api/0.1/plot
      imperial=on (optional)
      gpxdata=file attached (required)
    
    Response:
      either application/json on success: {'url':'chart-URL'}
      or 400 on error
    
    Incompatible API changes will change minor version number (0.2, 0.3, etc.).
    Backwards compatible changes may be numbered like 0.1.1, 0.1.2 etc.

--- online/index.py
+++ online/index.py
@@ -5,9 +5,34 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext.webapp import template
 
+from django.utils import simplejson as json
+
 import logging
 
 from gpxplot import parse_gpx_data,google_chart_url,var_dist,var_ele
+
+def plot_on_request(request):
+	"Process POST request with GPX data. Return a URL of the plot."
+	imperial=request.get('imperial')
+	if imperial == 'on':
+		metric=False
+	else:
+		metric=True
+	gpxdata=request.get("gpxfile")
+	if len(gpxdata) == 0:
+		raise Exception("There is no GPX data to plot!")
+	# reduce number of points gradually, to fit URL length
+	npoints=800
+	url=None
+	while not url:
+		try:
+			trk=parse_gpx_data(gpxdata,npoints=npoints)
+			url=google_chart_url(trk,var_dist,var_ele,metric=metric)
+		except OverflowError, e:
+			npoints -= 100
+			if npoints <= 0:
+				raise e
+	return url
 
 class MainPage(webapp.RequestHandler):
 	def get(self):
@@ -18,25 +43,7 @@ class MainPage(webapp.RequestHandler):
 	def post(self):
 		content={'title':'Elevation–distance profile','form':False, 'bg':'#fff'}
 		try:
-			imperial=self.request.get('imperial')
-			if imperial == 'on':
-				metric=False
-			else:
-				metric=True
-			gpxdata=self.request.get("gpxfile")
-			if len(gpxdata) == 0:
-				raise Exception("There is no GPX data to plot!")
-			# reduce number of points gradually, to fit URL length
-			npoints=800
-			url=None
-			while not url:
-				try:
-					trk=parse_gpx_data(gpxdata,npoints=npoints)
-					url=google_chart_url(trk,var_dist,var_ele,metric=metric)
-				except OverflowError, e:
-					npoints -= 100
-					if npoints <= 0:
-						raise e
+			url=plot_on_request(self.request)
 			content['imgsrc']=url
 		except Exception, e:
 			msg = 'Your GPX track cannot be processed. Sorry :-('
@@ -45,8 +52,19 @@ class MainPage(webapp.RequestHandler):
 			logging.error('Exception: '+unicode(e))
 		self.response.out.write(template.render('index.html',content))
 
+class ApiHandler(webapp.RequestHandler):
+	"Return a URL of the image in a file."
+	def post(self):
+		try:
+			url=plot_on_request(self.request)
+			self.response.headers['Content-Type']='application/json'
+			self.response.out.write(json.dumps({'url':url}))
+		except Exception, e:
+			self.response.set_status(400,message='Exception: '+unicode(e))
+
 application = webapp.WSGIApplication(
-		 [('/', MainPage)],
+		 [('/', MainPage),
+		  ('/api/0.1/plot',ApiHandler)],
 		 debug=True)
 
 def main():
